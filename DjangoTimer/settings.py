2dd683f84a5cc44e36289c75dba10566e567c1e6
0noder4 <bartosz.kuklewski@tm1.edu.pl>
2023-01-09 22:38:48 +0100

    feat: timer functionality, basic view

--- DjangoTimer/settings.py
+++ DjangoTimer/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Timer.apps.TimerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Timer/views.py
+++ Timer/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+import datetime
 
 def index(request):
-    return HttpResponse("Timer")
+    impreza = "21-01-01 17:30"
+    impreza = {
+        "dzien": 21,
+        "godzina": 17,
+        "minuta": 30
+    }
+
+    teraz = datetime.datetime.now()
+
+    godziny = impreza["godzina"] - teraz.hour + (impreza["dzien"] - teraz.day) * 24
+    minuty = impreza["minuta"] - teraz.minute
+
+    if minuty < 0:
+        godziny = godziny - 1
+        minuty += 60
+
+
+    return render(request, 'index.html', {'godziny': godziny, 'minuty': minuty})
